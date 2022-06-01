0a879eba77e5c5ce4bdf38d627503f536a486766
Gemar Usi <gusi@ph.indra.com>
2022-06-01 15:11:27 +0800

    edit file

--- hello.pl
+++ hello.pl
@@ -58,3 +58,21 @@ print "$long_string\n";
 
 my @abcs = ('a' .. 'z'); #array a - z 
 print join(", ", @abcs), "\n";
+
+my $customers = "Sue Sally Paul";
+my @cust_array = split / /, $customers;
+print join(", ", @cust_array), "\n";
+
+@cust_array = reverse @cust_array;
+
+@cust_array = reverse sort @cust_array;
+
+my @number_array = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+my @odd_array = grep {$_ % 2} @number_array;
+
+print join(", ", @odd_array), "\n";
+
+my @dbl_array = map {$_ * 2} @number_array;
+
+print join(", ", @dbl_array), "\n";
